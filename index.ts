97d47b2a541426688fc5f343c94b0e221a9d31c2
Joseph Maramba <josephmaramba50@gmail.com>
2022-03-25 10:27:10 +0300

    linear search algorithm

---
+++ index.ts
@@ -0,0 +1,24 @@
+/**
+ * LINEAR SEARCHINH
+ * returns the index position of the target, else returns -1 (not found)
+ */
+
+const linearSearch = (arr: number[], key: number): number => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === key) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+// verify results
+
+const verify = (index: number): string => {
+  return index === -1 ? `Key not found` : `Key found at position ${index}`;
+};
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const results = linearSearch(numbers, 10);
+
+console.log(verify(results));
